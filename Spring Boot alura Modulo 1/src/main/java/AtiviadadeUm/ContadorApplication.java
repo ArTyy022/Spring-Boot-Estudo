df1b2dd17b97ece800c50b371a6958b1635b1f4a
ArTyy022 <toledoarthur11@gmail.com>
2025-10-09 14:45:29 -0300

    Add files via upload

---
+++ Spring Boot alura Modulo 1/src/main/java/AtiviadadeUm/ContadorApplication.java
@@ -0,0 +1,26 @@
+package AtiviadadeUm;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class ContadorApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContadorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Digite um número: ");
+        Scanner entrada = new Scanner(System.in);
+        var numero = entrada.nextInt();
+
+        for(int i = 0; i<= numero; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
