ae88999c5f9a2067790ccb4f01cf012e01d36cbc
ArTyy022 <toledoarthur11@gmail.com>
2025-10-16 11:26:08 -0300

    Add files via upload

---
+++ Spring Boot alura Modulo 3/src/main/java/com/example/Modulo/Tres/AtividadeSete/Main.java
@@ -0,0 +1,20 @@
+package com.example.Modulo.Tres.AtividadeSete;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Produto> produtos = Arrays.asList(
+                new Produto("Smartphone", 800.0, "Eletrônicos"),
+                new Produto("Notebook", 1500.0, "Eletrônicos"),
+                new Produto("Teclado", 200.0, "Eletrônicos"),
+                new Produto("Cadeira", 300.0, "Móveis"),
+                new Produto("Monitor", 900.0, "Eletrônicos"),
+                new Produto("Mesa", 700.0, "Móveis")
+        );
+        produtos.stream()
+                .filter(p -> )
+
+    }
+}
