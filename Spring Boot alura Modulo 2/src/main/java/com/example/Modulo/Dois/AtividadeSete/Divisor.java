4f7cb9abcfca1ff955ce18b38b910924979f8190
ArTyy022 <toledoarthur11@gmail.com>
2025-10-16 10:19:24 -0300

    Add files via upload

---
+++ Spring Boot alura Modulo 2/src/main/java/com/example/Modulo/Dois/AtividadeSete/Divisor.java
@@ -0,0 +1,6 @@
+package com.example.Modulo.Dois.AtividadeSete;
+
+public interface Divisor {
+    int dividir(int a, int b) throws ArithmeticException;
+
+}
---
+++ Spring Boot alura Modulo 2/src/main/java/com/example/Modulo/Dois/AtividadeSete/Main.java
@@ -0,0 +1,19 @@
+package com.example.Modulo.Dois.AtividadeSete;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Divisor divisor = (a, b) -> {
+            if (b == 0) throw new ArithmeticException("Divisão por zero");
+            return a / b;
+        };
+
+        try {
+            System.out.println(divisor.dividir(10, 2));
+            System.out.println(divisor.dividir(10, 0));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
